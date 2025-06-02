08c97829b826ae36af534984121404b92ca47c66
rigs <rigorsyguatbsn@gmail.com>
2025-06-02 18:10:48 +0800

    update backend for production

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 const Pool = require("pg").Pool;
-require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
+  connectionString: process.env.DATABASE_URL,
+
   ssl: {
     rejectUnauthorized: false,
   },

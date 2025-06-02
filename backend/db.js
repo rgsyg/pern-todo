2033a2781ccebd7e3e684fa8e5491904c5bb35de
rigs <rigorsyguatbsn@gmail.com>
2025-06-02 13:53:23 +0800

    update db.js

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
   user: process.env.POSTGRES_USER || "postgres",
   password: process.env.POSTGRES_PASSWORD || "123",
   host: process.env.POSTGRES_HOST || "localhost",
-  port: 6543 || 5432,
+  port: 5432,
   database: process.env.POSTGRES_DATABASE || "my_todo",
 });
 

dcfa48ccf0cf5f2b27712a05612782876e224410
rigs <rigorsyguatbsn@gmail.com>
2025-06-02 16:19:30 +0800

    revert

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
+  user: process.env.POSTGRES_USER || "postgres",
+  password: process.env.POSTGRES_PASSWORD || "123",
+  host: process.env.POSTGRES_HOST || "localhost",
+  port: 5432,
+  database: process.env.POSTGRES_DATABASE || "my_todo",
   ssl: {
     rejectUnauthorized: false,
   },

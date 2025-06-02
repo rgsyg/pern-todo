7835cda925c1ea36975834a8d7eb9d1c390ac876
rigs <rigorsyguatbsn@gmail.com>
2025-06-02 16:35:23 +0800

    try

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,8 @@
 const Pool = require("pg").Pool;
+require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.POSTGRES_USER || "postgres",
-  password: process.env.POSTGRES_PASSWORD || "123",
-  host: process.env.POSTGRES_HOST || "localhost",
-  port: 5432,
-  database: process.env.POSTGRES_DATABASE || "my_todo",
+  connectionString: process.env.POSTGRES_URL,
   ssl: {
     rejectUnauthorized: false,
   },

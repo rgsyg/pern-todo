2e5b75a0d36e96b060c69ec01ce1132a2887a7da
rigs <rigorsyguatbsn@gmail.com>
2025-06-02 15:24:57 +0800

    update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: process.env.POSTGRES_USER || "postgres",
-  password: process.env.POSTGRES_PASSWORD || "123",
-  host: process.env.POSTGRES_HOST || "localhost",
-  port: 5432,
-  database: process.env.POSTGRES_DATABASE || "my_todo",
+  connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;

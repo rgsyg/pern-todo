a8d42ecf6ded27b96eb77bf8e818082871b81f66
rigs <rigorsyguatbsn@gmail.com>
2025-06-02 15:20:04 +0800

    revert

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "123",
-  host: "localhost",
+  user: process.env.POSTGRES_USER || "postgres",
+  password: process.env.POSTGRES_PASSWORD || "123",
+  host: process.env.POSTGRES_HOST || "localhost",
   port: 5432,
-  database: "my_todo",
+  database: process.env.POSTGRES_DATABASE || "my_todo",
 });
 
 module.exports = pool;
